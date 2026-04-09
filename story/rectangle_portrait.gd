8dbd3cc6f868810d7acdad0a5c2519c75e75f022
eepMoody <moody.designer@gmail.com>
2026-04-08 20:53:52 -0400

    set up initial dialogic example & readme update

---
+++ story/rectangle_portrait.gd
@@ -0,0 +1,7 @@
+extends DialogicPortrait
+
+func _get_covered_rect() -> Rect2:
+	return Rect2(0, 0, 140, 200)
+
+func _update_portrait(_character: DialogicCharacter, _portrait: String) -> void:
+	pass
